1f19e0ed02f5388c453e438bf7c7384e880afea4
hodori0111-arch <hodori0111@gmail.com>
2026-06-25 21:57:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-// 오프라인에서도 앱이 열리도록 하는 간단한 서비스워커
-const CACHE = 'mybudget-v1';
+// 오프라인에서도 앱이 열리도록 하는 서비스워커
+// 화면(HTML)은 '인터넷 먼저' → 새 버전이 바로 반영됨. 인터넷 없으면 저장본 사용.
+const CACHE = 'mybudget-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
@@ -15,11 +16,26 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
-      return res;
-    }).catch(() => caches.match('./index.html')))
-  );
+  const isHTML = e.request.mode === 'navigate' ||
+                 e.request.destination === 'document' ||
+                 e.request.url.endsWith('/index.html') ||
+                 e.request.url.endsWith('/');
+
+  if (isHTML) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match(e.request).then(hit => hit || caches.match('./index.html')))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }))
+    );
+  }
 });
